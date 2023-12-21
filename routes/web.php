bc4171be4eeb740e63589f68696a868907552ad1
YonanPrasetyo <yonanprasetyo91792@gmail.com>
2023-12-21 19:02:31 +0700

    ke-3-rev

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home',[
+        'name'=> "Yonan Prasetyo",
+        'role'=> "Staff",
+        'buah'=> ['pisang', 'apel', 'jeruk', 'semangka', 'kiwi']
+    ]);
 });
